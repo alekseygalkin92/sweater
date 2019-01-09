c014380d84afde0852fa6286b0cdab7e9bcf85ea
alekseygalkin <alekseygalkin92@gmail.com>
2019-01-09 16:50:26 +0300

    Add database

--- src/main/java/com/sweater/controller/HomeController.java
+++ src/main/java/com/sweater/controller/HomeController.java
@@ -1,18 +1,52 @@
 package com.sweater.controller;
 
+import com.sweater.domain.Message;
+import com.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String home(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
+    @Autowired
+    private MessageRepo messageRepo;
 
-        model.addAttribute("name", name);
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("messageList", messageRepo.findAll());
+        model.addAttribute("message", new Message());
+        return "home";
+    }
+
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute Message message, Model model) {
+        messageRepo.save(message);
+
+        model.addAttribute("messageList", messageRepo.findAll());
+        model.addAttribute("message", new Message());
+
+        return "home";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messageList", messages);
+        model.addAttribute("message", new Message());
+
         return "home";
     }
 }
