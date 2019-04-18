dcf0b085635bec5b573dcc5363204a88d1effaea
alekseygalkin <alekseygalkin92@gmail.com>
2019-04-18 16:30:14 +0300

    Add user's mail verification

--- src/main/java/com/sweater/controller/RegistrationController.java
+++ src/main/java/com/sweater/controller/RegistrationController.java
@@ -1,20 +1,18 @@
 package com.sweater.controller;
 
-import com.sweater.domain.Role;
 import com.sweater.domain.User;
-import com.sweater.repos.UserRepo;
+import com.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,15 +21,22 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null) {
+
+        if (!userService.addUser(user)) {
             model.addAttribute("message", "User exists");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
+
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isAvctivated = userService.activateUser(code);
+
+        model.addAttribute((isAvctivated ? "User successfully activated" : "Activation code is not found!"));
+
+        return "login";
     }
 }
